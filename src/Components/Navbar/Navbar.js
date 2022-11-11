92264200cfd14b42bfc280803b85cb1a346bdb8a
HasibAliOpu <wwwhasib4@gmail.com>
2022-11-11 15:41:28 +0600

    connecting with github and adding navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import user from "../../assets/user.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-gray-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+        </div>
+        <a href="/" className="btn btn-ghost normal-case text-xl">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <a>My Assignment</a>
+          </li>
+          <li>
+            <a>Chat with Mentor</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              <img src={user} alt="" />
+              ProfileName
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
