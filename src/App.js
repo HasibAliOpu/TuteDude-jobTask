d86fcfd4febb434c732d1b8855fc34463328573d
HasibAliOpu <wwwhasib4@gmail.com>
2022-11-11 15:47:55 +0600

    breadcrumbs added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Breadcrumbs from "./Components/Breadcrumbs/Breadcrumbs";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Breadcrumbs />
     </div>
   );
 }
---
+++ src/Components/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Breadcrumbs = () => {
+  return (
+    <div className="text-sm breadcrumbs">
+      <ul>
+        <li>
+          <a>UI/UX</a>
+        </li>
+        <li>
+          <a>Refer & Earn</a>
+        </li>
+        <li>Friends Referred</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
